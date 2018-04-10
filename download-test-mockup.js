91c254c6ec881e697ff0f67b7ceb6c6a6c4ce281
Luca Pellegrini <aroldo93@hotmail.it>
2018-04-10 18:44:15 +0200

    Sostituito XMLHttpRequest con Fetch API. Su Firefox funziona correttamente. Modificato index.html (avevo dimenticato di fare alcune modifiche nel precedente commit).

--- download-test-mockup.js
+++ download-test-mockup.js
@@ -3,17 +3,37 @@ var host = "192.168.1.180:60100";
 var totalBytes = 0;
 var startTime = Date.now();
 var xhr;
+var go = true;
 
 var downloadStream = function() {
+	var req = {
+		request:'download',
+		data_length: bytesToDownload
+	};
+
+	var jsonReq = JSON.stringify(req);
+	var url = 'http://' + host + '?r=' + Math.random() + "&data=" + encodeURIComponent(jsonReq);
+	var fetchRequest = new Request(url);
+	fetch(fetchRequest)
+		.then(function(response) {
+			return response.blob();
+		}).then(function(blob) {
+			totalBytes += blob.size;
+			console.log("loaded");
+			if(go) {
+				downloadStream();
+			}
+		});
 	
+	/*
 	xhr = new XMLHttpRequest();
 
 	var prevBytes = 0;
-	/*
+	
 	xhr.onprogress = function(event) {
 		addBytes(event.loaded);
 		console.log("progress");
-	};*/
+	};
 	
 	xhr.onload = function(event) {
 		xhr.abort();
@@ -38,7 +58,7 @@ var downloadStream = function() {
 	var jsonReq = JSON.stringify(req);
 	var url = 'http://' + host + '?r=' + Math.random() + "&data=" + encodeURIComponent(jsonReq);
 	xhr.open('GET', url);
-	xhr.send();
+	xhr.send();*/
 }
 
 downloadStream();
@@ -47,10 +67,11 @@ var interval = setInterval(function() {
 	var time = Date.now();
 	var elapsedTime = time - startTime;
 	var speedInMbps = (totalBytes/125)/elapsedTime;
-	postMessage(speedInMbps.toString() + " " + totalBytes.toString());
+	postMessage({'speed': speedInMbps, 'bytes': totalBytes});
 	console.log(time);
 	if(elapsedTime > 10000) {
-		xhr.abort();
+		//xhr.abort();
+		go = false;;
 		clearInterval(interval);
 		close();
 	}
